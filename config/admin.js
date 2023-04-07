5f8a8f43319a6c252246f94da569a0f5f62a946e
BingChilllling <119667055+BingChilllling@users.noreply.github.com>
2023-04-07 16:55:56 -0400

    defined apiToken.salt

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'fNrOwmQ3+BOVpmeH4icDZQ=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '5a/PdoU8MPMuRc7Argn2uQ=='),
   },
 });
